aa22463b44d0be1ee6031e0f3c5ccf6a436d86a2
Titou <titouan.hochet@epitech.eu>
2026-08-31 21:57:43 +0200

    feat(backend/application modules): init the application module

---
+++ backend/src/applications/applications.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ApplicationsService } from './applications.service';
+import { ApplicationsController } from './applications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Application } from './entities/application.entity';
+import { Job } from '../jobs/entities/job.entity';
+import { User } from '../users/entities/user.entity';
+
+@Module({
+  controllers: [ApplicationsController],
+  providers: [ApplicationsService],
+  imports: [TypeOrmModule.forFeature([Application, Job, User])],
+  exports: [TypeOrmModule]
+})
+export class ApplicationsModule {}
