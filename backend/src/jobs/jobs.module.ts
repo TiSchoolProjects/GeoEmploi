27e3ee80d998988bf7404b6c900bec534b1e87a8
Titou <titouan.hochet@epitech.eu>
2026-08-31 21:59:43 +0200

    feat(backend/jobs): init of the job module

---
+++ backend/src/jobs/jobs.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { JobsController } from './jobs.controller';
+import { Job } from './entities/job.entity';
+import { User } from '../users/entities/user.entity';
+import { Application } from '../applications/entities/application.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  controllers: [JobsController],
+  providers: [JobsService],
+  imports: [TypeOrmModule.forFeature([Job, User, Application])],
+  exports: [TypeOrmModule]
+
+})
+export class JobsModule {}
