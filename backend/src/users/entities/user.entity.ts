952c847b2a33a42298d701c63cdec543ca638dfe
Titou <titouan.hochet@epitech.eu>
2026-08-31 22:29:51 +0200

    feat(User/entity) add the status variable to the table

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,10 +1,18 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToOne } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToOne, OneToMany } from "typeorm";
 import { Employer } from "../../employers/entities/employer.entity";
+import { Seeker } from "../../seekers/entities/seeker.entity";
+import { Job } from "../../jobs/entities/job.entity";
+import { Application } from "../../applications/entities/application.entity";
 
 export enum UserRole {
   SEEKER = 'seeker',
   EMPLOYER = 'employer',
   ADMIN = 'admin',
+}
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  SUSPENDED = 'suspended'
 }
 
 @Entity()
@@ -24,9 +32,22 @@ export class User {
   @Column({type: 'enum', enum: UserRole, default: UserRole.SEEKER})
   role: UserRole;
 
+  @Column({type: 'enum', enum: UserStatus, default: UserStatus.ACTIVE})
+  status: UserStatus;
+
   @CreateDateColumn()
   createdAt: Date;
 
   @OneToOne('Employer', (employer: Employer) => employer.user)
   employerProfile: Employer
+
+  @OneToOne('Seeker', (seeker: Seeker) => seeker.user)
+  seekerProfile: Seeker
+
+  @OneToMany('Job', (job: Job) => job.employer)
+  jobs: Job[];
+
+  @OneToMany('Application', (app : Application) => app.jobSeeker)
+  applications : Application[];
+  
 }
