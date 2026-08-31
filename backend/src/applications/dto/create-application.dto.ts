2091f651ff89a2b18324cb7b005fe47b151837e1
Titou <titouan.hochet@epitech.eu>
2026-08-31 23:40:15 +0200

    feat(backend/application): add validation pipe and check of the type given to add more error handling

--- backend/src/applications/dto/create-application.dto.ts
+++ backend/src/applications/dto/create-application.dto.ts
@@ -1,1 +1,11 @@
-export class CreateApplicationDto {}
+import { IsNumber, IsNotEmpty } from 'class-validator';
+
+export class CreateApplicationDto {
+  @IsNumber()
+  @IsNotEmpty()
+  jobSeekerId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  jobId: number;
+}
--- backend/src/applications/dto/update-application.dto.ts
+++ backend/src/applications/dto/update-application.dto.ts
@@ -1,4 +1,11 @@
+import { IsEnum } from 'class-validator';
+import { ApplicationStatus } from '../entities/application.entity';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateApplicationDto } from './create-application.dto';
 
 export class UpdateApplicationDto extends PartialType(CreateApplicationDto) {}
+
+export class UpdateApplicationStatusDto {
+  @IsEnum(ApplicationStatus)
+  status: ApplicationStatus;
+}
