8cf25c5482e76c4e3b538fac7fae04bf8a4aa61f
Titou <titouan.hochet@epitech.eu>
2026-08-31 22:30:39 +0200

    feat(backend/user): add the new tables to the user module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,11 +4,14 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Employer } from '../employers/entities/employer.entity';
+import { Seeker } from '../seekers/entities/seeker.entity';
+import { Job } from '../jobs/entities/job.entity';
+import { Application } from '../applications/entities/application.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User, Employer])],
+  imports: [TypeOrmModule.forFeature([User, Employer, Seeker, Job, Application])],
   exports: [TypeOrmModule]
 })
 export class UsersModule {}
