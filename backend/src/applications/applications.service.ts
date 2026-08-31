b5127d9fd9c9ce6ac57cf577c11b41beaa73904f
Titou <titouan.hochet@epitech.eu>
2026-08-31 23:07:25 +0200

    feat(backend/applications): add all the functions for the application module

--- backend/src/applications/applications.service.ts
+++ backend/src/applications/applications.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { CreateApplicationDto } from './dto/create-application.dto';
-import { UpdateApplicationDto } from './dto/update-application.dto';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Application, ApplicationStatus } from './entities/application.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ApplicationsService {
-  create(createApplicationDto: CreateApplicationDto) {
-    return 'This action adds a new application';
+  constructor(
+    @InjectRepository(Application) 
+    private AppRepository: Repository<Application>,
+  ) {}
+
+  async apply(jobSeekerId: number, jobId: number): Promise<Application> {
+    const exist = await this.AppRepository.findOne({where : {jobSeekerId, jobId}});
+
+    if (exist) {
+      throw new ConflictException("Vous avez déja postulé.");
+    }
+
+    const app = this.AppRepository.create({jobSeekerId, jobId, status: ApplicationStatus.WAITING});
+
+    return this.AppRepository.save(app);
   }
 
   findAll() {
-    return `This action returns all applications`;
+    return this.AppRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} application`;
+  async findbySeekerId(jobSeekerId: number): Promise<Application[]> {
+    return this.AppRepository.find({ where: {jobSeekerId}, relations: {jobSeeker: true}, order: {createdAt: 'DESC'},
+    });
   }
 
-  update(id: number, updateApplicationDto: UpdateApplicationDto) {
-    return `This action updates a #${id} application`;
+  async findbyJobId(jobId: number): Promise<Application[]> {
+    return this.AppRepository.find({ where: {jobId}, relations: {jobSeeker: true}, order: {createdAt: 'DESC'},
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} application`;
+  async UpdateStatus(id: number, status: ApplicationStatus): Promise<Application> {
+    const app = await this.AppRepository.findOne({where: {id}});
+
+    if (!app) {
+      throw new NotFoundException("Candidature non trouvée.");
+    }
+
+    app.status = status;
+    return this.AppRepository.save(app);
+  }
+
+  async remove(id: number, jobSeekerId: number): Promise<void> {
+    const app = await this.AppRepository.findOne({where: {id, jobSeekerId}, });
+
+    if (!app) {
+      throw new NotFoundException('Candidature non trouvée.');
+    }
+
+    await this.AppRepository.remove(app);
   }
 }
