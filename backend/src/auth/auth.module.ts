237cc6d2f9f9a42393570f954ded2a738c43ee5c
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-01 17:42:12 +0200

    feat(backend/authentication): deploy the global app guard to protect routes by default

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,9 +6,12 @@ import { JwtStrategy } from './jwt.strategy.js';
 import { LocalStrategy } from './local.strategy.js';
 import { LocalAuthGuard } from './local-auth.guard.js';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './jwt-auth.guard.js';
+import { APP_GUARD } from '@nestjs/core';
 import { StringValue } from 'ms';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller.js';
+
 
 @Module({
   imports: [
@@ -23,8 +26,16 @@ import { AuthController } from './auth.controller.js';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, LocalAuthGuard, JwtStrategy],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },
+    AuthService,
+    LocalStrategy,
+    LocalAuthGuard,
+    JwtStrategy
+  ],
   exports: [AuthService, PassportModule, LocalAuthGuard]
 })
 
-export class AuthModule {}
+export class AuthModule { }
