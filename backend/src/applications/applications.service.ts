c95046dcee79065b3eb9088cc05d6d0339585ea4
Titou <titouan.hochet@epitech.eu>
2026-09-01 11:40:10 +0200

    fix(backend/application): add a forgotten await to prevent multiple request sent at one time

--- backend/src/applications/applications.service.ts
+++ backend/src/applications/applications.service.ts
@@ -48,7 +48,7 @@ export class ApplicationsService {
     }
 
     app.status = status;
-    return this.AppRepository.save(app);
+    return await this.AppRepository.save(app);
   }
 
   async remove(id: number): Promise<void> {
