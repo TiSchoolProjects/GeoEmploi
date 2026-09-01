344e891d7c45ed81e164965c81ef308bdfe88e14
Titou <titouan.hochet@epitech.eu>
2026-09-01 11:55:11 +0200

    feat(backend/employer): add the controller and the verification of the types

--- backend/src/employers/dto/create-employer.dto.ts
+++ backend/src/employers/dto/create-employer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateEmployerDto {}
+import { IsNumber, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateEmployerDto {
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  companyName: string;
+
+  @IsString()
+  companyDesc: string;
+}
