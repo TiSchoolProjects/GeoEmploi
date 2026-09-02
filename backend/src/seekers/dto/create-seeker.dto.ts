ab2722e6358707ef12cb3c78b80340bf61c4f36e
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-02 15:46:58 +0200

    feat(backend/seeker): add documentation for seeker endpoints

--- backend/src/seekers/dto/create-seeker.dto.ts
+++ backend/src/seekers/dto/create-seeker.dto.ts
@@ -1,19 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class CreateSeekerDto {
-
+  @ApiProperty({ description: 'Unique user id', example: 12345})
   @IsNumber()
   @IsNotEmpty()
   userId: number;
 
+  @ApiProperty({ description: 'Array of skills', example: ['Management', 'Python', 'Fullstack']})
   @IsArray()
   @IsString({ each: true})
   @IsNotEmpty()
   skills: string[];
 
+  @ApiProperty({ description: 'Job experience', example: '2 years'})
   @IsString()
   experience: string;
 
+  @ApiProperty({ description: 'Time until user is available', example: 'Immediately'})
   @IsString()
   availability: string;
 }
