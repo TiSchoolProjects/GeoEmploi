fd64afe8c21bc1832571e17248a18436fa34db6c
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-02 14:55:12 +0200

    feat(backend/user): add documentation for user endpoints

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'Email address', example: 'yourmail@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Account password', example: 'very-secret-password', writeOnly: true })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ description: 'User\'s first and last name', example: 'John Doe' })
   @IsString()
   @IsNotEmpty()
   username: string;
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,31 +1,37 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { createDoc, findAllDoc, findbyEmailDoc, findOneDoc, removeDoc } from './user.controller.doc';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
+  @createDoc()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @findAllDoc()
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @findOneDoc()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @findbyEmailDoc()
   @Get('/email/:id')
   findByEmail(@Param('email') email: string) {
     return this.usersService.findbyEmail(email);
   }
 
+  @removeDoc()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
