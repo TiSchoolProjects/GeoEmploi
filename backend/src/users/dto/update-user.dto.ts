435b7f2ccc96fdf5ecdbaa4b25ed0f28fb464941
Titou <titouan.hochet@epitech.eu>
2026-09-03 14:56:28 +0200

    feat(backend/user): store first name and last name instead of username

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,11 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { IsEnum } from 'class-validator';
-import { UserRole } from '../entities/user.entity';
+import { UserStatus } from '../entities/user.entity';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
 
-export class UpdateApplicationStatusDto {
-  @IsEnum(UserRole)
-  status: UserRole;
+export class UpdateStatusDto {
+  @IsEnum(UserStatus)
+  status: UserStatus;
 }
