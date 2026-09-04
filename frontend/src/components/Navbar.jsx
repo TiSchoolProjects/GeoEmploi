ac33b7b4cc6dd654d53a92cb4e474ab8b2368771
Titou <titouan.hochet@epitech.eu>
2026-09-04 09:32:11 +0200

    fix(frontend): remove access to a unfinished feature

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,9 +20,9 @@ function NavBar() {
       <div className="nav-right">
         {/*<Link to="/Cgu" className="nav-link">À propos</Link>*/}
 
-        /*{user?.role === "seeker" && (
+        {/*user?.role === "seeker" && (
           <Link to="/my-application" className="nav-link">Candidatures</Link>
-        )}*/
+        )*/}
         {user?.role === "employer" && (
           <div className="employer-actions">
             <Link to="/my-job-offers" className="nav-link">Voir mes offres</Link>
