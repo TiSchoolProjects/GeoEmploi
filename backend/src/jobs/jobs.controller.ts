704061f7aac4928c2726270494a1267311322449
Titou <titouan.hochet@epitech.eu>
2026-09-07 17:44:01 +0200

    feat(back/jobs): add the routes

--- backend/src/jobs/jobs.controller.ts
+++ backend/src/jobs/jobs.controller.ts
@@ -94,4 +94,10 @@ export class JobsController {
     return this.jobsService.remove(id, req.user.userId, req.user.role);
   }
 
+  @Roles(UserRole.ADMIN, UserRole.EMPLOYER)
+  @Patch('views/:id')
+  increaseView(@Param('id', ParseIntPipe) id: number) {
+    this.increaseView(id);
+  }
+
 }
