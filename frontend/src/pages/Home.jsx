75ad3df59409622d593ac0083646df60d513d3b0
ari <marion.mousset@epitech.eu>
2026-09-08 14:41:48 +0200

    feat(i18n): add internationalization support with English and French translations

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,22 +6,24 @@ import NavBar from "../components/Navbar";
 import Footer from "../components/Footer"
 import LocLogo from "../assets/LogoCarte.png";
 
+import { useTranslation } from "react-i18next";
+
 
 function Home() {
+  const {t} = useTranslation()
   return (
     <div className="home">
-a
       {/* NAVBAR */}
       <NavBar />
       {/* BODY */}
       <main className="body-container">
         <div className="body-style-container">
-          <p>Découvrez le marché <br /> de l'emploi français</p>
+          <p>{t("home.title")}<br />{t("home.subtitle")}</p>
           <div className="map-button-container">
-            <Link to="/map" className="map-btn"> <span>Découvrir la carte</span> <span className="arrow">→</span> </Link>
+            <Link to="/map" className="map-btn"> <span>{t("home.mapButton")}</span> <span className="arrow">→</span> </Link>
           </div>
         </div>
-        <div className="image-container"> <img src={LocLogo} alt="Illustration" /></div>
+        <div className="image-container"> <img src={LocLogo} alt={t("home.imageAlt")} /></div>
       </main>
     </div>
   );
