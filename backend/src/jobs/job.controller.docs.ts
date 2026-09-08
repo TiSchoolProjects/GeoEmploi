5786f1bd1ccb561ec46434d7ad4db52ffe18a89a
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-08 15:55:44 +0200

    feat(backend/documentation): update jobs controller documentation

--- backend/src/jobs/job.controller.docs.ts
+++ backend/src/jobs/job.controller.docs.ts
@@ -28,6 +28,9 @@ export function findAllDoc() {
 export function findAroundDoc() {
     return applyDecorators(
         ApiOperation({ summary: 'Get all jobs in given radius from given position' }),
+        ApiQuery({ name: 'lat', description: 'Latitude' }),
+        ApiQuery({ name: 'lng', description: 'Longitude' }),
+        ApiQuery({ name: 'radius', description: 'Radius' }),
         ApiParam({
             name: 'searchJobDto',
             type: SearchJobDto,
@@ -40,6 +43,7 @@ export function findAroundDoc() {
 export function findByEmployerDoc() {
     return applyDecorators(
         ApiOperation({ summary: 'Get all jobs posted by given employer' }),
+        ApiParam({ name: 'id', description: 'Employer id' }),
         ApiResponse({ status: 200, description: 'List of all jobs from employer', type: [Job] }),
         ApiResponse({ status: 404, description: 'No employer found with given id', type: undefined })
     );
@@ -59,13 +63,15 @@ export function findAdminDoc() {
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Find all job offers that need their geocoding be verified' }),
         ApiResponse({ status: 200, description: 'All corresponding job offers', type: [Job] }),
-
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
     )
 }
 
 export function findOneDoc() {
     return applyDecorators(
         ApiOperation({ summary: 'Find an job through their id' }),
+        ApiParam({ name: 'id', description: 'Job id'}),
         ApiResponse({ status: 200, description: 'Found job info', type: Job }),
         ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
@@ -75,7 +81,10 @@ export function updateDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Update job info' }),
+        ApiParam({ name: 'id', description: 'Job id'}),
         ApiResponse({ status: 200, description: 'Updated job info', type: Job }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
 }
@@ -84,7 +93,10 @@ export function archiveDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Archive job, making it unavailable for viewing or application' }),
+        ApiParam({ name: 'id', description: 'Job id'}),
         ApiResponse({ status: 200, description: 'Archived job', type: Job }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
 }
@@ -93,7 +105,10 @@ export function removeDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Remove job from database' }),
+        ApiParam({ name: 'id', description: 'Job id'}),
         ApiResponse({ status: 200, description: 'Removed job', type: DeleteResult }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
 }
@@ -101,6 +116,7 @@ export function removeDoc() {
 export function increaseViewDoc() {
     return applyDecorators(
         ApiOperation({ summary: 'Increase view by one on an offer' }),
+        ApiParam({ name: 'id', description: 'Job id'}),
         ApiResponse({ status: 200, description: 'Increased view', type: UpdateResult }),
         ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
