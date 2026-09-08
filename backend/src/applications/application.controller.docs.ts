c05103fd1365680389169f437c1980138dbfa003
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-08 16:52:21 +0200

    feat(backend/documentation): update applications controller documentation

--- backend/src/applications/application.controller.docs.ts
+++ backend/src/applications/application.controller.docs.ts
@@ -15,6 +15,9 @@ export function applyDoc() {
             description: 'Application information',
         }),
         ApiResponse({ status: 201, description: 'Application info', type: Application }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
+        ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
 }
 
@@ -23,6 +26,9 @@ export function findAllDoc() {
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Get all applications' }),
         ApiResponse({ status: 200, description: 'List of all applications', type: [Application] }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
+        ApiResponse({ status: 404, description: 'No application found with given id', type: undefined })
     );
 }
 
@@ -30,7 +36,10 @@ export function findOneDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Find an application through their id' }),
+        ApiParam({ name: 'id', description: 'Application id' }),
         ApiResponse({ status: 200, description: 'Found application info', type: Application }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No application found with given id', type: undefined })
     );
 }
@@ -39,7 +48,10 @@ export function findBySeekerDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Find an application through seeker id' }),
+        ApiParam({ name: 'id', description: 'Seeker id' }),
         ApiResponse({ status: 200, description: 'List of all applications from seeker', type: [Application] }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No seeker found with given id', type: undefined })
     );
 }
@@ -48,8 +60,11 @@ export function findByJobDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Find an application through job id' }),
+        ApiParam({ name: 'id', description: 'Job id' }),
         ApiResponse({ status: 200, description: 'List of all applications from employer', type: [Application] }),
-        ApiResponse({ status: 404, description: 'No employer found with given id', type: undefined })
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
+        ApiResponse({ status: 404, description: 'No job found with given id', type: undefined })
     );
 }
 
@@ -57,12 +72,15 @@ export function updateStatusDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Archive application, making it unavailable for viewing or application' }),
+        ApiParam({ name: 'id', description: 'Application id' }),
         ApiParam({
             name: 'updateApplicationDto',
             type: UpdateApplicationDto,
             description: 'Application information',
         }),
         ApiResponse({ status: 200, description: 'Archived application', type: Application }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No application found with given id', type: undefined })
     );
 }
@@ -71,7 +89,10 @@ export function removeDoc() {
     return applyDecorators(
         ApiBearerAuth('JWT-Auth'),
         ApiOperation({ summary: 'Remove application from database' }),
+        ApiParam({ name: 'id', description: 'Application id' }),
         ApiResponse({ status: 200, description: 'Removed application', type: DeleteResult }),
+        ApiResponse({ status: 401, description: 'Invalid credentials', type: undefined }),
+        ApiResponse({ status: 403, description: 'Missing permissions', type: undefined }),
         ApiResponse({ status: 404, description: 'No application found with given id', type: undefined })
     );
 }
