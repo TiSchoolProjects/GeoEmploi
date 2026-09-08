927d620fce1c9b1f13d7e034f73d8e54928d008c
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-08 15:24:20 +0200

    fix(backend/messages): update all error/informative messages

--- backend/src/applications/applications.service.ts
+++ backend/src/applications/applications.service.ts
@@ -31,7 +31,7 @@ export class ApplicationsService {
     const job = await this.JobRepository.findOne({ where: { id: jobId }, });
 
     if (!job) {
-      throw new NotFoundException("L'offre demandée n'existe pas.");
+      throw new NotFoundException("Offre non trouvée.");
     }
 
     const exist = await this.AppRepository.findOne({ where: { jobSeekerId, jobId } });
@@ -70,7 +70,7 @@ export class ApplicationsService {
 
     if ((currentUser.role === UserRole.SEEKER && currentUser.userId !== application.jobSeekerId)
       || (currentUser.role === UserRole.EMPLOYER && currentUser.userId !== employerId)) {
-      throw new ForbiddenException("You do not own this resource.");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     return application;
@@ -98,7 +98,7 @@ export class ApplicationsService {
     const employerId = await this.getEmployerId(jobId);
 
     if (currentUser.role === UserRole.EMPLOYER && currentUser.userId !== employerId) {
-      throw new ForbiddenException("You do not own this resource.");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     return applications;
@@ -120,7 +120,7 @@ export class ApplicationsService {
     const employerId = await this.getEmployerId(app.jobId);
 
     if (currentUser.role === UserRole.EMPLOYER && currentUser.userId !== employerId) {
-      throw new ForbiddenException("You do not own this resource.");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     return await this.AppRepository.save(app);
@@ -139,7 +139,7 @@ export class ApplicationsService {
     }
 
     if (currentUser.role === UserRole.SEEKER && currentUser.userId !== application.jobSeekerId) {
-      throw new ForbiddenException("You do not own this resource.");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     await this.AppRepository.remove(application);
--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -109,10 +109,10 @@ export class JobsService {
     const job = await this.jobRepository.findOne({ where: { id } });
 
     if (!job) {
-      throw new NotFoundException("Offre non trouvé.");
+      throw new NotFoundException("Offre non trouvée.");
     }
     if (role !== UserRole.ADMIN && job.employerId !== curId) {
-      throw new ForbiddenException("Vous ne pouvez pas modifié cette offre");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     const changed = updateJobDto.commune !== undefined && updateJobDto.commune != job.commune;
@@ -143,7 +143,7 @@ export class JobsService {
     }
 
     if (role !== UserRole.ADMIN && job.employerId !== curId) {
-      throw new ForbiddenException("Cette offre ne vous appartient pas.");
+      throw new ForbiddenException("Vous n'avez pas la permission.");
     }
 
     await this.jobRepository.remove(job);
