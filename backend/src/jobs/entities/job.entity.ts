4169554fdf71acc176c9615b5b327cd124c31078
Gabriel Viel <gabriel.viel@epitech.eu>
2026-09-08 11:45:40 +0200

    fix(backend/documentation): change last example addresse to municipality precision

--- backend/src/jobs/entities/job.entity.ts
+++ backend/src/jobs/entities/job.entity.ts
@@ -30,7 +30,7 @@ export class Job {
   @Column('text')
   description: string;
 
-  @ApiProperty({ description: 'Job address', example: '123 Elm Street, New York, NY 10001'})
+  @ApiProperty({ description: 'Job address', example: 'Rennes, 35000'})
   @Column({name: 'adress'})
   commune: string;
 
